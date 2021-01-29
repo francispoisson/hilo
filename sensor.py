4b33acc0e2cde819065e4b02969a8aa768ed0d01
francispoisson <francis.poisson2@gmail.com>
2021-01-28 19:09:05 -0500

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -8,25 +8,21 @@ from datetime import timedelta
 
 from . import DOMAIN
 
-SCAN_INTERVAL = timedelta(seconds=60)
+#SCAN_INTERVAL = timedelta(seconds=60)
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
-    h = hass.data[DOMAIN]
-    
-    for i in range(len(h.d)):
-        if(h.d[i].deviceType == 'Meter'):
-            add_entities([PowerSensor(h, h.d[i],i)])
+            
+    for i in range(len(hass.data[DOMAIN].d)):
+        add_entities([PowerSensor(hass.data[DOMAIN], i)])
 
 class PowerSensor(Entity):
     """Representation of a sensor."""
 
-    def __init__(self, h, d, index):
+    def __init__(self, h, index):
         self.index = index
-        self.entity_id = ENTITY_ID_FORMAT.format(d.deviceId)
-        self._name = d.name
-        
-        self._d = d
+        self.entity_id = ENTITY_ID_FORMAT.format(h.d[index].deviceId)
+        self._name = h.d[index].name
         
         self._h = h
         
@@ -35,12 +31,12 @@ class PowerSensor(Entity):
     @property
     def name(self):
         """Return the precision of the system."""
-        return self._d.name
+        return self._h.d[self.index].name
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._d.Power
+        return self._h.d[self.index].Power
     
     @property    
     def device_class(self):
@@ -53,7 +49,6 @@ class PowerSensor(Entity):
         return POWER_WATT
 
     def update(self):
-        self._h.update_device(self.index)
-        self._d = self._h.d[self.index] 
+        self._h.update()
 
-        self._state  = self._d.Power
+        self._state  = self._h.d[self.index].Power
