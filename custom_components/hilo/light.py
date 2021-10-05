fb62af0a12d3430ff70311b17e9e3a0b4eb6d94a
francispoisson <francis.poisson2@gmail.com>
2021-10-05 08:48:38 -0400

    Update light.py

--- custom_components/hilo/light.py
+++ custom_components/hilo/light.py
@@ -12,7 +12,7 @@ SCAN_IMTERVAL = timedelta(seconds=15)
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     for i in range(len(hass.data[DOMAIN].d)):
-        if(hass.data[DOMAIN].d[i].deviceType == 'LightDimmer'):
+        if(hass.data[DOMAIN].d[i].deviceType == 'LightDimmer' or hass.data[DOMAIN].d[i].deviceType == 'WhiteBulb' or hass.data[DOMAIN].d[i].deviceType == 'ColorBulb'):
             add_entities([HiloDimmer(hass.data[DOMAIN], i)])
 
     return True
